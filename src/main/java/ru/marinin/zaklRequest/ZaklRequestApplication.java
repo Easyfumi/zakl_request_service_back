04cba839f838c0529c5b205f8c1f466d2b4596f7
Easyfumi <153363694+Easyfumi@users.noreply.github.com>
2024-07-02 07:10:01 +0300

    Initial commit

---
+++ src/main/java/ru/marinin/zaklRequest/ZaklRequestApplication.java
@@ -0,0 +1,13 @@
+package ru.marinin.zaklRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaklRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaklRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/marinin/zaklRequest/ZaklRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.marinin.zaklRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaklRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
