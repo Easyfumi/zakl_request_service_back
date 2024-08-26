70ddef97f43ee751aeae9db5e82b89473731a958
Oleg <oleg.marinin.49@yandex.ru>
2024-08-26 20:07:14 +0300

    request sample 'new' and 'ext.' download links

--- src/main/java/ru/marinin/zaklRequest/controllers/FileController.java
+++ src/main/java/ru/marinin/zaklRequest/controllers/FileController.java
@@ -21,10 +21,10 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 public class FileController {
 
-    @GetMapping("/downloadRequestSample")
-    public ResponseEntity<byte[]> downloadRequestSample() {
+    @GetMapping("/downloadRequestSampleNew")
+    public ResponseEntity<byte[]> downloadRequestSampleNew() {
 
-                Path path = Paths.get("src/main/resources/static/files/Заявка_Правила_распространение Sollers SCV дизель.docx");
+                Path path = Paths.get("src/main/resources/static/files/Заявка_Правила_получение.docx");
 
         byte[] content = null;
         try {
@@ -33,7 +33,23 @@ public class FileController {
         }
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
-        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("Request_Sample_NAMI.docx").build().toString());
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("Request_Sample_NAMI_new.docx").build().toString());
+        return ResponseEntity.ok().headers(httpHeaders).body(content);
+    }
+
+    @GetMapping("/downloadRequestSampleExt")
+    public ResponseEntity<byte[]> downloadRequestSampleExt() {
+
+        Path path = Paths.get("src/main/resources/static/files/Заявка_Правила_распространение.docx");
+
+        byte[] content = null;
+        try {
+            content = Files.readAllBytes(path);
+        } catch (final IOException e) {
+        }
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        httpHeaders.set(HttpHeaders.CONTENT_DISPOSITION, ContentDisposition.attachment().filename("Request_Sample_NAMI_ext.docx").build().toString());
         return ResponseEntity.ok().headers(httpHeaders).body(content);
     }
 }
